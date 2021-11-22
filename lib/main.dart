d435d860bae902a0ae0cc180bafba98b763c69cb
yungproger <dchinasylov@mail.ru>
2021-11-22 16:33:10 +0600

    skelet

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-
-
-
+import 'package:apc_project/ui/home/home_page.dart';
 import 'package:flutter/cupertino.dart';
 
-void main() => runApp(app);
+void main() => runApp(HomePage());
---
+++ lib/ui/home/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  _buildBody() {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _buildBody(),
+    );
+  }
+}
