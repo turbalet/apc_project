c094902b6a28c790810137ee4ac0820435b90677
yungproger <dchinasylov@mail.ru>
2021-11-22 17:28:53 +0600

    skelet

---
+++ lib/foundation/constants.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+const backgroundColor = Color(0xDA000000);
+const primary = Color(0xFFAF95FF);
+const primaryWithOp = Color(0xA6AF95FF);
+const backgroundItem = Color(0x476F737B);
+const grey = Color(0xFFB7B7B7);
+const white = Color(0xFFFFFFFF);
+const darkGrey = Color(0xFF6F737B);
+const red = Color(0xFFEB4335);
+const progressBorder = Color(0xB8000000);
+const leaveGrey = Color(0x9E6F737B);
+const leaveBackground = Color(0x826F737B);
+const white2 = Color(0xABFFFFFF);
+const whiteScore = Color(0x30FFFFFF);
+const whiteLock = Color(0x8FFFFFFF);
+const whiteInputBorder = Color(0x66FFFFFF);
